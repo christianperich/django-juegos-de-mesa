eb50361145e33d5b482f68a620fb4197c6e1e40c
Chris Perich <christianperich@gmail.com>
2024-02-05 21:24:04 -0800

    DB models created

--- juegos/models.py
+++ juegos/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Tienda(models.Model):
+    nombre = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100, default='No indicado')
+
+    def __str__(self):
+        return self.nombre
+    
+    
+class Juego(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+
+class Detalles(models.Model):
+    tienda = models.ForeignKey(Tienda, on_delete=models.CASCADE)
+    juego = models.ForeignKey(Juego, on_delete=models.CASCADE)
+    precio = models.IntegerField()
+    url = models.URLField()
+    imagen_url = models.URLField()
+
+    def __str__(self):
+        return f'{{self.juego}} - {{self.tienda}}'
+    
